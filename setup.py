8bdc76a9a7c47782e92739deab66b978a438bba4
Frank Henard <frank@mirusresearch.com>
2018-03-21 13:50:12 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mirus_django_csv",
-    version="1.1.0.dev0",
-    description="Takes a django queryset, or a list, or a generator and creates a csv from it",
-    author="Mirus Research",
-    author_email="frank@mirusresearch.com",
+    name='mirus_django_csv',
+    version='1.1.0',
+    description='Takes a django queryset, or a list, or a generator and creates a csv from it',
+    author='Mirus Research',
+    author_email='frank@mirusresearch.com',
     packages=find_packages(),
     url='https://github.com/mirusresearch/mirus_django_csv',
     license='MIT license, see LICENSE',
-    py_modules=["mirus_django_csv"],
+    py_modules=['mirus_django_csv'],
     install_requires=[
         'six>=1.11.0',
     ],
